3189ba62a79743aecd20c1541e99ee6758ebea85
Raul Alzate <raul.alzate@techandsolve.com>
2016-06-03 17:12:18 -0500

    se avalua los conceptos de compoenet, module y scope de Dagger 2

--- app/src/main/java/com/example/raalzate/exampledagger2/App.java
+++ app/src/main/java/com/example/raalzate/exampledagger2/App.java
@@ -3,22 +3,37 @@ package com.example.raalzate.exampledagger2;
 import android.app.Application;
 import android.content.Context;
 
-import com.example.raalzate.exampledagger2.component.DaggerDiComponent;
-import com.example.raalzate.exampledagger2.component.DiComponent;
+import com.example.raalzate.exampledagger2.di.components.DaggerGitHubComponent;
+import com.example.raalzate.exampledagger2.di.components.DaggerNetComponent;
+import com.example.raalzate.exampledagger2.di.components.GitHubComponent;
+import com.example.raalzate.exampledagger2.di.components.NetComponent;
+import com.example.raalzate.exampledagger2.di.modules.GitHubModule;
+import com.example.raalzate.exampledagger2.di.modules.AppModule;
+import com.example.raalzate.exampledagger2.di.modules.NetModule;
 
 /**
  * Created by raul-alzate on 3/06/16.
  */
 public class App extends Application {
-    private DiComponent component;
+    private NetComponent netComponent;
+    private GitHubComponent apiGitHubComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        component = DaggerDiComponent.builder().build();
+
+       netComponent = DaggerNetComponent.builder()
+                .appModule(new AppModule(this))
+                .netModule(new NetModule("https://api.github.com"))
+                .build();
+
+        apiGitHubComponent = DaggerGitHubComponent.builder()
+                .netComponent(netComponent)
+                .gitHubModule(new GitHubModule())
+                .build();
     }
 
-    public static DiComponent getDiComponent(Context context) {
-        return ((App) context.getApplicationContext()).component;
+    public static GitHubComponent getComponent(Context context) {
+        return ((App) context.getApplicationContext()).apiGitHubComponent;
     }
 }
